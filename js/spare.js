bd08c1b5cb1c8af91abd9e204ef8444d68473f2a
Frank Mitchell <me@frankmitchell.org>
2014-08-16 10:21:22 -0400

    Solves for initial rules regarding picking pins.

--- js/spare.js
+++ js/spare.js
@@ -144,22 +144,101 @@ function bowl (value) {
   frame += 1
 }
 
+function allPinsVisible () {
+  var i = 0
+  for (i = 0; i < 9; i += 1) {
+    if ($('#pin'+i).has('hidden')) {
+      return false
+    }
+  }
+  return true
+}
+
+function adjacentPins (value) {
+  switch (value) {
+    case 0: return [1, 4]
+    case 1: return [0, 2, 4, 5]
+    case 2: return [1, 3, 5, 6]
+    case 3: return [2, 6]
+    case 4: return [0, 1, 5, 7]
+    case 5: return [1, 2, 4, 6, 7, 8]
+    case 6: return [2, 3, 5, 8]
+    case 7: return [4, 5, 8, 9]
+    case 8: return [5, 6, 7, 9]
+    case 9: return [7, 8]
+    default: return []
+  }
+}
+
+function isAdjacentPin (a, b) {
+  return adjacentPins(a).indexOf(b) > -1
+}
+
+Array.prototype.equals = function (array) {
+  var i = 0
+
+  if (!array) {
+    return false
+  }
+  if (this.length !== array.length) {
+    return false
+  }
+  for (i = 0; i < this.length; i += 1) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i])) {
+        return false
+      }
+    } else if (this[i] !== array[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function canPickPin (value) {
+  if (allPinsVisible()) {
+    if (pins.equals([])) { return [9,8,7,6,4].indexOf(value) > -1 }
+    if (pins.equals([9])) { return [8,7].indexOf(value) > -1 }
+    if (pins.equals([8])) { return [9,7,6].indexOf(value) > -1 }
+    if (pins.equals([7])) { return [9,8,4].indexOf(value) > -1 }
+    if (pins.equals([6])) { return value === 8 }
+    if (pins.equals([4])) { return value === 7 }
+    if (pins.sort().equals([8,9])) { return [7,6].indexOf(value) > -1 }
+    if (pins.sort().equals([7,9])) { return [8,4].indexOf(value) > -1 }
+    if (pins.sort().equals([7,8])) { return [9,6,4].indexOf(value) > -1 }
+    if (pins.sort().equals([6,8])) { return [9,7].indexOf(value) > -1 }
+    if (pins.sort().equals([4,7])) { return [9,8].indexOf(value) > -1 }
+  }
+  return pins.length < 3
+}
+
+function canUnpickPin (value) {
+  if (allPinsVisible()) {
+    if (pins.sort().equals([4,7,8])) { return value !== 7 }
+    if (pins.sort().equals([4,7,9])) { return value !== 7 }
+    if (pins.sort().equals([6,7,8])) { return value !== 8 }
+    if (pins.sort().equals([6,8,9])) { return value !== 8 }
+  }
+  return pins.indexOf(value) > -1
+}
+
 function onPin (event) {
   var target = $(event.srcElement || event.target)
-    , index = 0
+  console.log(pins)
 
   if (target.has('picked')) {
-    index = pins.indexOf(target.data())
-    if (index > -1) {
-      pins.splice(index, 1)
+    if (canUnpickPin(target.data())) {
+      pins.splice(pins.indexOf(target.data()), 1)
       target.toggle('picked')
     }
   } else {
-    if (pins.length < 3) {
-      target.toggle('picked')
+    if (canPickPin(target.data())) {
       pins.push(target.data())
+      target.toggle('picked')
     }
   }
+
+  console.log(pins)
 }
 
 function onBall (event) {
