d0d20566c76c89d1a26a61ea5f29494b2693395f
Frank Mitchell <me@frankmitchell.org>
2014-08-24 11:09:46 -0400

    Fixes a bug where touch events weren't snappy on mobile devices.

--- js/jquery.js
+++ js/jquery.js
@@ -175,35 +175,37 @@ Fn.prototype.animate = function (klass, callback) {
 
 Fn.prototype.touch = function (start, end) {
   if (this.element) {
-    this.element.onmousedown = function (event) {
-      if (start) {
-        start(event)
-      }
-      document.onmousemove = function (event) {
-        event.preventDefault()
-      }
-      document.onmouseup = function (event) {
-        if (end) {
-          end(event)
+    if (!'ontouchstart' in document.documentElement) {
+      this.element.onmousedown = function (event) {
+        if (start) {
+          start(event)
         }
-        document.onmousemove = null
-        document.onmouseup = null
-      }
-    }
-    this.element.ontouchstart = function (event) {
-      this.element.onmousedown = null
-      if (start) {
-        start(event)
-      }
-      document.ontouchmove = function (event) {
-        event.preventDefault()
-      }
-      document.ontouchend = function (event) {
-        if (end) {
-          end(event)
+        document.onmousemove = function (event) {
+          event.preventDefault()
         }
-        document.ontouchmove = null
-        document.ontouchend = null
+        document.onmouseup = function (event) {
+          if (end) {
+            end(event)
+          }
+          document.onmousemove = null
+          document.onmouseup = null
+        }
+      }
+    } else {
+      this.element.ontouchstart = function (event) {
+        if (start) {
+          start(event)
+        }
+        document.ontouchmove = function (event) {
+          event.preventDefault()
+        }
+        document.ontouchend = function (event) {
+          if (end) {
+            end(event)
+          }
+          document.ontouchmove = null
+          document.ontouchend = null
+        }
       }
     }
   }
